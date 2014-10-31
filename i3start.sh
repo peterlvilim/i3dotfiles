3de3e4bd902f867043c0fa21092c01575b966675
Peter Vilim <peterlvilim@users.noreply.github.com>
2014-10-30 22:30:29 -0500

    rmdropboxandredshift

--- i3start.sh
+++ i3start.sh
@@ -1,7 +1,6 @@
 xsetroot -solid "#001b26" &
 xset -b &
 xautolock -time 15 -locker ~/.i3/lock.sh &
-redshift -l 40.659:88.1435 -t 3700:6500 &
-dropbox start &
+redshift -l 40.659:88.1435 -t 5000:6500 &
 xinput disable 11 &
 ~/.i3/music.sh &
