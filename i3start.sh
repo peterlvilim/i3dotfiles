e98f82ccabf528ad57c426c26a749d05a32b9860
Peter Vilim <peterlvilim@users.noreply.github.com>
2014-10-30 19:34:53 -0500

    autolock

--- i3start.sh
+++ i3start.sh
@@ -1,6 +1,7 @@
 xsetroot -solid "#001b26" &
 xset -b &
-xautolock -time 15 -locker ~/.i3/lock.sh &
+xautolock -time 3 -locker ~/.i3/lock.sh &
+xautolock -time 15 -locker ~/.i3/suspend.sh &
 redshift -l 40.659:88.1435 -t 3700:6500 &
 dropbox start &
 xinput disable 11 &
