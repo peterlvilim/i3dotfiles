0587bfb6c70807c7ad0c07fb8f63954ccb9746b0
Peter Vilim <peterlvilim@users.noreply.github.com>
2014-11-08 03:06:06 -0600

    Sync against hydrogen

--- i3start.sh
+++ i3start.sh
@@ -2,7 +2,7 @@ xsetroot -solid "#001b26" &
 xset -b &
 xautolock -time 3 -locker ~/.i3/lock.sh &
 xautolock -time 15 -locker ~/.i3/suspend.sh &
-redshift -l 40.659:88.1435 -t 3700:6500 &
+redshift -l 40.659:88.1435 -t 5000:6500 &
 dropbox start &
 xinput disable 11 &
 ~/.i3/music.sh &
