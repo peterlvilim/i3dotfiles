0332f9db00ae5e2977f17e1ec5771127d2e668ee
Peter Vilim <peterlvilim@users.noreply.github.com>
2014-12-25 21:00:53 -0600

    i3start

--- i3exit.sh
+++ i3exit.sh
@@ -2,3 +2,5 @@ killall -9 redshift
 i3-msg exit
 dropbox stop
 pkill -9 -f mopidy
+pkill -9 -f syncthing
+logout
--- i3start.sh
+++ i3start.sh
@@ -3,7 +3,7 @@ xset -b &
 xautolock -time 3 -locker ~/.i3/lock.sh &
 xautolock -time 15 -locker ~/.i3/suspend.sh &
 redshift -l 40.659:88.1435 -t 5000:6500 &
-dropbox start &
 xinput disable 11 &
 ~/.i3/music.sh &
 unclutter &
+syncthing &
