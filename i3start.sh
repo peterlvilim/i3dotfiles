c69acf8935d25bbebe244e1ece9a787374d8b1ba
Peter Vilim <peterlvilim@users.noreply.github.com>
2014-12-29 01:56:34 -0600

    i3start

--- i3start.sh
+++ i3start.sh
@@ -1,7 +1,6 @@
 xsetroot -solid "#001b26" &
 xset -b &
-xautolock -time 3 -locker ~/.i3/lock.sh &
-xautolock -time 15 -locker ~/.i3/suspend.sh &
+xautolock -time 5 -locker ~/.i3/suspend.sh &
 redshift -l 40.659:88.1435 -t 5000:6500 &
 xinput disable 11 &
 ~/.i3/music.sh &
