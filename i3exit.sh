dc379ca7bea910420bd96597f301989cd9726933
Peter Vilim <peterlvilim@users.noreply.github.com>
2014-12-28 19:06:31 -0600

    syncthing: remove

--- i3exit.sh
+++ i3exit.sh
@@ -2,5 +2,4 @@ killall -9 redshift
 i3-msg exit
 dropbox stop
 pkill -9 -f mopidy
-pkill -9 -f syncthing
 logout
--- i3start.sh
+++ i3start.sh
@@ -6,4 +6,3 @@ redshift -l 40.659:88.1435 -t 5000:6500 &
 xinput disable 11 &
 ~/.i3/music.sh &
 unclutter &
-syncthing &
