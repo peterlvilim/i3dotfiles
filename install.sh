5ee8b5cc98dbfc8de9f5afbe6f5ce758665e9e62
Peter Vilim <peter.vilim@delphix.com>
2015-03-08 00:31:14 -0800

    install

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
-ln -s /home/pvilim/.i3/.xinitrc /home/pvilim/.xinitrc
-ln -s /home/pvilim/.i3/.Xdefaults /home/pvilim/.Xdefaults
+ln -f -s /home/pvilim/.i3/.xinitrc /home/pvilim/.xinitrc
+ln -f -s /home/pvilim/.i3/.Xdefaults /home/pvilim/.Xdefaults
+ln -f -s /home/pvilim/.i3/.Xmodmap /home/pvilim/.Xmodmap
