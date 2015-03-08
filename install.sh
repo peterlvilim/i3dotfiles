a60d362ab9f7d4b0b0f230837337bee317670ae4
Peter Vilim <peter.vilim@delphix.com>
2015-03-08 16:36:29 -0700

    fixdmenu

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 ln -f -s /home/pvilim/.i3/.xinitrc /home/pvilim/.xinitrc
 ln -f -s /home/pvilim/.i3/.Xdefaults /home/pvilim/.Xdefaults
 ln -f -s /home/pvilim/.i3/.Xmodmap /home/pvilim/.Xmodmap
+ln -f -s /home/pvilim/.i3/dmenu-dark /home/pvilim/bin/dmenu-dark
