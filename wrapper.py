a4a5a3b8752602742264b04139b539aad87c01df
Peter Vilim <peter.vilim@delphix.com>
2015-03-12 19:53:55 -0500

    wrapper

--- wrapper.py
+++ wrapper.py
@@ -120,7 +120,6 @@ def read_line():
 
 def monitor_hotplug():
     commands.getstatusoutput("/home/pvilim/bin/dohotplug DP-3")
-    commands.getstatusoutput("/home/pvilim/bin/dohotplug DP-4")
 
 
 if __name__ == '__main__':
